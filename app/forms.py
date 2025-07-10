5cd3ba4a11537f5d90ce6c1999c9fa49301c93c7
sanvit <katrekarsanvit2@gmail.com>
2025-07-10 17:26:01 +0400

    fix: remove registration validation

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 ''' Forms for when User has to enter details '''
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import InputRequired, EqualTo, Length, Regexp
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo
 from app.constants import *
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators=[InputRequired(), Length(min=4, max=30)])
-	password = PasswordField('Password', validators=[InputRequired(), Length(min=6)])
+	username = StringField('Username', validators=[InputRequired()])
+	password = PasswordField('Password', validators=[InputRequired()])
 	confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
 	submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[InputRequired(), Length(min=4, max=30)])
+	username = StringField('Username', validators=[InputRequired()])
 	password = PasswordField('Password', validators = [InputRequired()])
